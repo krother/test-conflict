2f6b8272e3cc2d3277c225506ad6fd466b31d18f
Kristian Rother <kristian.rother@posteo.de>
2026-04-22 13:37:53 +0200

    edit line 8

--- grey-scale.py
+++ grey-scale.py
@@ -5,7 +5,7 @@ a = np.zeros((200, 200, 3), dtype=np.uint8)
 a += 128
 # lets add some green squares
 a[50:100, 50:100] = (0, 255, 0)
-a[100:150, 100:150] = (0, 255, 0)
+a[100:150, 100:150] = (0, 255, 120)
 
 im = Image.fromarray(a)
 im.save('gray.png')
